d0030d9b279588012afd6963f30c9380ea588c2c
Kyelee Fitts <krf2135@columbia.edu>
2022-06-24 01:15:39 +0000

    almost done

---
+++ week1/project_assessment.md
@@ -0,0 +1,33 @@
+## Project Assessment - Week 1
+
+1. Do you understand the steps involved in creating and deploying an LTR model?  Name them and describe what each step does in your own words.
+
+First, you gather relevance judgements from search logs (whether that's from click data or human judgements). Then, you extract features for each query-document pair in your dataset. These can be query-dependent or independent. Then, you split your data into training and test sets. Then, you train an XGBoost (or other) ranking model on the training set, and use search evaluation metrics like nDCG on the test set to evaluate how you ranked. 
+
+2. What is a feature and featureset?
+
+A feature (in this context) is a value which can provide some information as to how relevant a document is to a query. A featureset is a json file which contains the features to be extracted from query logs. 
+
+3. What is the difference between precision and recall?
+
+Precision: out of all the results returned, what fraction are relevant?
+
+Recall: out of all the possibly relevant results, what fraction were returned? 
+
+4. What are some of the traps associated with using click data in your model?
+
+Different biases: presentation (people will tend to click on the first thing), uncertainty (is 1 impression and 1 click enough to say that your relevance judgement is 1?), and overall data/tracking issues. 
+
+5. What are some of the ways we are faking our data and how would you prevent that in your application?
+
+We faked impressions data. We should probably use real impressions data. 
+
+6. What is target leakage and why is it a bad thing?
+
+When you leak some of the testing data into the training data, causing your model to overfit. 
+
+7. When can using prior history cause problems in search and LTR?
+
+Search patterns can change over time. Past data may not be the best indicator of current search trends. 
+
+Best MRR score: 
--- week1/utilities/data_prepper.py
+++ week1/utilities/data_prepper.py
@@ -6,6 +6,7 @@ import pandas as pd
 import query_utils as qu
 from opensearchpy import RequestError
 import os
+from collections import defaultdict
 
 # from importlib import reload
 
@@ -232,29 +233,16 @@ class DataPrepper:
                                                 size=len(query_doc_ids), terms_field=terms_field)
         ##### Step Extract LTR Logged Features:
         response = self.opensearch.search(body=log_query, index=self.index_name)
-        # features = response['hits']['hits'][0]['fields']['_ltrlog'][0]['log_entry']
-        print("PRINTING:", response['hits']['hits'][0]['_id'])
-        # Loop over the hits structure returned by running `log_query` and then extract out the features from the response per query_id and doc id.  Also capture and return all query/doc pairs that didn't return features
-        # Your structure should look like the data frame below
-        feature_dict = {}
-        for doc in response['hits']['hits'][0]['_id']:
-            for feat in response['hits']['hits'][0]['fields']['_ltrlog'][0]['log_entry']:
-                feature_dict[feat['name']] = feat['name']
-
-        feature_results = {}
-        feature_results["doc_id"] = []  # capture the doc id so we can join later
-        feature_results["query_id"] = []  # ^^^
-        feature_results["sku"] = []
-        feature_results["name_match"] = []
-        rng = np.random.default_rng(12345)
-        for doc_id in query_doc_ids:
-            feature_results["doc_id"].append(doc_id)  # capture the doc id so we can join later
-            feature_results["query_id"].append(query_id)
-            feature_results["sku"].append(doc_id)  
-            feature_results["name_match"].append(rng.random())
-        frame = pd.DataFrame(feature_results)
-        return frame.astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
-        # IMPLEMENT_END
+        feature_results = defaultdict(list)
+        hit = response['hits']['hits'][0]
+        doc_id = hit["_id"]
+        for feat in hit['fields']['_ltrlog'][0]['log_entry']:
+            feature_results[feat['name']].append(feat.get('value', 0))
+        feature_results["doc_id"].append(doc_id)
+        feature_results["query_id"].append(query_id)
+        feature_results["sku"].append(doc_id)
+        # print(pd.DataFrame(feature_results)[['short_description_phrase_match', 'long_description_phrase_match', 'artist_name_phrase_match']])
+        return pd.DataFrame(feature_results).astype({'doc_id': 'int64', 'query_id': 'int64', 'sku': 'int64'})
 
     # Can try out normalizing data, but for XGb, you really don't have to since it is just finding splits
     def normalize_data(self, ranks_features_df, feature_set, normalize_type_map):
